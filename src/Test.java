d233b3ffc9754d6f18086212a06c36a826b9b1aa
LiMaoyuan <lima0yuan@163.com>
2023-11-02 21:02:47 +0800

    修改Test.java

--- src/Test.java
+++ src/Test.java
@@ -8,13 +8,16 @@ import java.util.Scanner;
 
 public class Test {
     public static void run(){
-        String command;
-        Scanner scanner = new Scanner(System.in);
-        while(true){
-            command = scanner.nextLine();
-            if(command.equals("exit"))
-                break;
-            Invoker.invoke(command);
+        String filePath = "input.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(">"+line);
+                Invoker.invoke(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
         }
 
     }
