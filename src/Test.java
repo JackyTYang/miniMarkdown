e135ec9bd4b9406908ab0da9abcc9e7f17fcbbcd
朱泽勤 <970022251@qq.com>
2023-11-02 22:00:16 +0800

    merge测试

--- src/Test.java
+++ src/Test.java
@@ -8,16 +8,25 @@ import java.util.Scanner;
 
 public class Test {
     public static void run(){
-        String filePath = "input.txt";
+//        String filePath = "input.txt";
+//
+//        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+//            String line;
+//            while ((line = br.readLine()) != null) {
+//                System.out.println(">"+line);
+//                Invoker.invoke(line);
+//            }
+//        } catch (IOException e) {
+//            System.err.println("Error reading the file: " + e.getMessage());
+//        }
 
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(">"+line);
-                Invoker.invoke(line);
-            }
-        } catch (IOException e) {
-            System.err.println("Error reading the file: " + e.getMessage());
+        String command;
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            command = scanner.nextLine();
+            if(command.equals("exit"))
+                break;
+            Invoker.invoke(command);
         }
 
     }
