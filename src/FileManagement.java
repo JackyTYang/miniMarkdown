bcd01940114ee5bba63de52ce1011077cf97c79b
Li Maoyuan <93751248+PoliteCircle@users.noreply.github.com>
2023-11-02 19:43:19 +0800

    重建目录结构

--- src/FileManagement.java
+++
@@ -1,9 +0,0 @@
-public class FileManagement {
-    public static void loadFile(String dir) {
-
-    }
-
-    public static void saveFile(){
-
-    }
-}
--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //Client.run();
-        Test.run();
-    }
-}
